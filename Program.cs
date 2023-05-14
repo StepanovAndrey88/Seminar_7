6a0d8d73675f151fe2afbc51a82b909230d66b7e
Stepanov Andrey <StepanovAndrey88@gmail.com>
2023-05-14 23:48:54 +0300

    Добавил ДЗ №7

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿##Домашнее задание № 7
+
+// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во колонок");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] array =new double [rows,columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    array[i,j] = new Random().Next(-99,99)/10.0;
+    // array[i,j] = i+j;
+    Console.Write(array[i,j]+ " ");
+    }
+
+    Console.WriteLine();
+}
+ 
+// Задача 50. Напишите программу, 
+// которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во колонок");
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] array =new int [rows,columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i,j] = new Random().Next(0,10);
+Console.Write(array[i,j]+ " ");
+}
+
+Console.WriteLine();
+}
+Console.WriteLine("Введите номер строки искомого числа");
+int Fdrows = Convert.ToInt32(Console.ReadLine())-1;
+
+Console.WriteLine("Введите номер столбца искомого числа");
+int Fdcolums = Convert.ToInt32(Console.ReadLine())-1;
+if (Fdrows < array.GetLength(0) && Fdcolums < array.GetLength(1))
+Console.WriteLine(array[Fdrows, Fdcolums]);
+else
+Console.WriteLine($"{Fdrows}{Fdcolums} -> такого числа в массиве нет");
+ 
+// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во колонок");
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] array =new int [rows,columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i,j] = new Random().Next(0,10);
+Console.Write(array[i,j]+ " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine(" ");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+double sum = 0;
+double avgNumbers = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+sum += array[i, j];
+}
+avgNumbers=Math.Round(sum/array.GetLength(0),1);
+Console.Write($"{avgNumbers} ");
+}
+
